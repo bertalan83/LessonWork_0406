3e30688330658f6ae99816963e2ecd5d0eb785e9
bertalan83 <e.bertalan96@gmail.com>
2022-04-06 18:00:21 +0200

    greatestcommondivisor method completed

--- src/main/java/main/tasks/GreatestCommonDivisor.java
+++ src/main/java/main/tasks/GreatestCommonDivisor.java
@@ -5,8 +5,17 @@ public class GreatestCommonDivisor {
     public static void main(String[] args) {
         int n1 = 0, n2 = 0;
 
-        int smaller = (n1 < n2 ? n1 : n2);
-        int bigger = (n1 > n2 ? n1 : n2);
+
+        System.out.println(findGCD(n1,n2));
+    }
+
+    /**
+     * Megtalálja két nemnegatív egész szám legnagyobb közös osztóját.
+     */
+    public static int findGCD(int number1, int number2) {
+
+        int smaller = (number1 < number2 ? number1 : number2);
+        int bigger = (number1 > number2 ? number1 : number2);
 
         while (smaller != bigger) {
             int sub = bigger - smaller;
@@ -15,15 +24,7 @@ public class GreatestCommonDivisor {
             smaller = (smaller < sub ? smaller : sub);
         }
 
-        System.out.println(smaller);
-    }
-
-    /**
-     * Megtalálja két nemnegatív egész szám legnagyobb közös osztóját.
-     */
-    public static int findGCD(int number1, int number2) {
-        // write your code here
-        return -1;
+        return smaller;
     }
 
 }

a77c31c8757b34d7e40d07a6a1b232ea334ce247
bertalan83 <e.bertalan96@gmail.com>
2022-04-06 18:02:56 +0200

    mirrorarray method completed

--- src/main/java/main/tasks/MirrorArray.java
+++ src/main/java/main/tasks/MirrorArray.java
@@ -5,18 +5,8 @@ public class MirrorArray {
     public static void main(String[] args) {
         int[] numbers = {1, 2, 2, 3, 4, 5, 5, 4, 3, 2, 2, 1};
 
-        boolean isMirrorArray = numbers.length > 0;
 
-        for (int i = 0; i < numbers.length / 2; i++) {
-            int iFromEnd = numbers.length - 1 - i;
-
-            if (numbers[i] != numbers[iFromEnd]) {
-                isMirrorArray = false;
-                break;
-            }
-        }
-
-        System.out.println("Is the array a mirror array? " + isMirrorArray);
+        System.out.println("Is the array a mirror array? " + isMirrorArray(numbers));
     }
 
     /**
@@ -35,8 +25,19 @@ public class MirrorArray {
      *      [1, 2, 3, 1]
      */
     public static boolean isMirrorArray(int[] array) {
-        // write your code here
-        return false;
+
+        boolean isMirrorArray = array.length > 0;
+
+        for (int i = 0; i < array.length / 2; i++) {
+            int iFromEnd = array.length - 1 - i;
+
+            if (array[i] != array[iFromEnd]) {
+                isMirrorArray = false;
+                break;
+            }
+        }
+
+        return isMirrorArray;
     }
 
 }

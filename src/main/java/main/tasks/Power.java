4e662e8d1c50b78f87a2455e4021f3b630b32e9f
bertalan83 <e.bertalan96@gmail.com>
2022-04-06 18:09:02 +0200

    power method completed

--- src/main/java/main/tasks/Power.java
+++ src/main/java/main/tasks/Power.java
@@ -5,21 +5,21 @@ public class Power {
     public static void main(String[] args) {
         int base = 0, exponent = 0;
 
-        int result = 1;
-
-        for (int i = 0; i < exponent; i++) {
-            result *= base;
-        }
-
-        System.out.println(result);
+        System.out.println(power(base,exponent));
     }
 
     /**
      * Hatványozást végez: a base értékét az exponent kitevőre emeli.
      */
     public static int power(int base, int exponent) {
-        // write your code here
-        return -1;
+
+        int result = 1;
+
+        for (int i = 0; i < exponent; i++) {
+            result *= base;
+        }
+
+        return result;
     }
 
 }
